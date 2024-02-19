8c8b24e071256424aa6a6f409a443deefe9ff13c
Matthew <mskwak01@korea.ac.kr>
2024-02-19 01:29:29 +0000

    New arrange

--- threestudio/models/geometry/__init__.py
+++
@@ -1,8 +0,0 @@
-from . import (
-    base,
-    custom_mesh,
-    implicit_sdf,
-    implicit_volume,
-    tetrahedra_sdf_grid,
-    volume_grid,
-)
--- threestudio/models/renderers/__init__.py
+++
@@ -1,9 +0,0 @@
-from . import (
-    base,
-    deferred_volume_renderer,
-    gan_volume_renderer,
-    nerf_volume_renderer,
-    neus_volume_renderer,
-    nvdiff_rasterizer,
-    patch_renderer,
-)
